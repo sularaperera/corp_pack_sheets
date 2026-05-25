a909a04118fbb8596fa26e96acd7c239de091df8
Sulara Perera <sulara.perera@outlook.com>
2026-05-25 15:22:12 +1200

    Add files via upload

--- app.py
+++ app.py
@@ -36,13 +36,18 @@ dsd_file = st.file_uploader(
 # -----------------------------
 def read_file(uploaded_file):
 
+    uploaded_file.seek(0)
+
     file_extension = Path(uploaded_file.name).suffix.lower()
 
     if file_extension == ".csv":
         return pd.read_csv(uploaded_file)
 
     elif file_extension == ".xlsx":
-        return pd.read_excel(uploaded_file)
+        return pd.read_excel(uploaded_file, engine="openpyxl")
+    
+    elif file_extension == ".xls":
+        return pd.read_excel(uploaded_file, engine="xlrd")
 
     else:
         raise ValueError("Unsupported file type")
